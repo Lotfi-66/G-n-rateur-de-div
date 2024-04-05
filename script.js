6c39630003ae96ceca854339db01084888d4604f
Format_Pro <lotfi.djeg@gmail.com>
2024-04-05 11:21:04 +0200

    ABCDE

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 //rgb(255, 255, 0)=> jaune
 //on crée une fonction qui génere en chiffre entre 0 et 255
-function randomNumber(){
+function randomNumber() {
     return Math.floor(Math.random() * 256);
 }
 
 //function qui retourne une couleur aléatoire en rgb
-function randomColor(){
-    return "rgb("+ randomNumber() + "," + randomNumber() + "," + randomNumber() +")";
+function randomColor() {
+    return "rgb(" + randomNumber() + "," + randomNumber() + "," + randomNumber() + ")";
 }
-
-
-
-
-
 
 //créé une fonction qui va générer une div
 function addDiv() {
@@ -29,6 +24,22 @@ function addDiv() {
     container.appendChild(newDiv);
 }
 
+//fonction enleve la derniere div
+function removeLastDiv() {
+    //on récupère le container
+    let container = document.getElementById('container');
+    let boxes = document.getElementsByClassName('box');
+    if (boxes.length > 0) {
+        //si j'ai au moins une div de créée j'enlève la dernière
+        container.removeChild(boxes[boxes.length - 1]);
+    }
+}
+
+//fonction qui supprime toutes les divs
+function removeAll(){
+    let container = document.getElementById('container');
+    container.innerHTML = "";
+}
 
 //on ajoute un evenement a notre bouton avec l'id "addButton"
 //je recupère le bouton avc l'id "addButton"
@@ -38,3 +49,16 @@ addButton.addEventListener("click", () => {
     //on appelle notre fonction addDiv
     addDiv();
 });
+
+//on ajoute un evenement a notre bouton avec l'id "removeLastButton"
+document.getElementById('removeLastButton').addEventListener("click", () => {
+    removeLastDiv();
+})
+
+//on ajoute un evenement a notre bouton avec l'id "removeAllDiv"
+document.getElementById('removeAllDiv').addEventListener("click", () => {
+    removeAll();
+})
+
+
+
