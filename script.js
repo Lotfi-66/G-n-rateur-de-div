e73765e24740d42f16f11173ffef7682e6f00888
Format_Pro <lotfi.djeg@gmail.com>
2024-04-05 10:38:36 +0200

    ABCDE

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//rgb(255, 255, 0)=> jaune
+//on crée une fonction qui génere en chiffre entre 0 et 255
+function randomNumber(){
+    return Math.floor(Math.random() * 256);
+}
+
+//function qui retourne une couleur aléatoire en rgb
+function randomColor(){
+    return "rgb("+ randomNumber() + "," + randomNumber() + "," + randomNumber() +")";
+}
+
+
+
+
+
+
 //créé une fonction qui va générer une div
 function addDiv() {
     //on récupere la div avec l'id container
@@ -8,7 +24,7 @@ function addDiv() {
     //newDiv.classList.add("box"); //1ère méthode
     newDiv.className = 'box'; //2ème méthode
     //on va ajouter un style a notre div => <div class="box" style="background-color: red"></div>
-    newDiv.style.backgroundColor = 'red';
+    newDiv.style.backgroundColor = randomColor();
     //on va imbriquer la nouvelle div dans la div container
     container.appendChild(newDiv);
 }
